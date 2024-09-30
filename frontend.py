4559489baa9c5d40cd38fc233b50802269656128
restallu <74469347+restallu@users.noreply.github.com>
2024-09-30 20:49:40 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -104,7 +104,7 @@ def printHeader(model,tokenizer):
 
 
 f1='./modelorob.pth'
-modelo=torch.load(f1,map_location=torch.device('cpu'))
+modelo=torch.load(f1,map_location=torch.device('cpu'),weights_only=True)
 f2='tokrob.pth'
 tokenizer=torch.load(f2)
 modelo.eval()
