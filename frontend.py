3e27488979df9c4582f373329fd5b22a31cdf447
restallu <74469347+restallu@users.noreply.github.com>
2024-09-30 21:36:33 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -70,7 +70,7 @@ def printHeader(model,tokenizer):
             }
 
         div[data-testid="stFileUploaderDropzoneInstructions"] > div > small::before {
-        visibility: visible;
+        visibility: hidden;
         content: "Límite de 10K por archivo";
         }
         </style>
