ea572af4ee9731c82d95c80ded0c31001e27f102
restallu <74469347+restallu@users.noreply.github.com>
2024-09-30 18:39:35 +0200

    Add files via upload

---
+++ frontend.py
@@ -0,0 +1,141 @@
+import streamlit as st 
+import pickle
+import torch
+import time 
+urlm="https://drive.google.com/file/d/1y4JI6ECzOs5E2vDs2JA8bsw-kk-iOgMW/view?usp=sharing"
+MAX_SEQ=400 
+PCMIN=0.4 #Porcentaje mínimo de la seq_max para que sea analizado
+import io
+import requests
+import urllib.request
+from transformers import RobertaForSequenceClassification
+PRE_TRAINED_MODEL_NAME='PlanTL-GOB-ES/roberta-base-bne'
+rutaRaiz="C:\\Master BD ENyD\\10-TFM"
+
+def formatContent(textoLargo):
+    textoLargoWords=textoLargo.split()
+    textoLargoLista=[]
+    while len(textoLargoWords)>=MAX_SEQ: 
+        textoLargoLista.append(' '.join(textoLargoWords[:MAX_SEQ-1])) 
+        textoLargoWords=textoLargoWords[MAX_SEQ:]
+    if len(textoLargoWords)>MAX_SEQ*PCMIN:
+        textoLargoLista.append(' '.join(textoLargoWords))
+    return textoLargoLista
+    
+def descargar_url(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        contenido = response.content
+        bytes_io = io.BytesIO(contenido)
+        st.write('descargando')
+        #return torch.load(bytes_io,map_location=torch.device('cpu'))
+        return torch.hub.load_state_dict_from_url()
+    else:
+        raise Exception(f"No se pudo descargar el archivo. Código de estado: {response.status_code}")
+    
+def read_remote_url(url):   
+    max_bytes = 2**31 - 1
+    bytes_in = bytearray()
+    
+    with urllib.request.urlopen(url) as response:
+        while True:
+            chunk = response.read(max_bytes)
+            if not chunk:
+                break
+            bytes_in += chunk
+    bytes_io = io.BytesIO(bytes_in)
+    return bytes_io
+    #return torch.load(bytes_io,map_location=torch.device('cpu'))
+
+    
+     
+def spinnerWidget(model,tokenizer,text_area):
+     with st.spinner('La frase tiene un sentimiento de.... '):    
+        if torch.cuda.is_available():
+            device = torch.device("cpu")
+            model.to(device)
+        else:
+            device = torch.device("cpu")
+        resultado=predict(model,tokenizer,text_area)
+        if resultado == 1:
+            texto1='Derecha'
+        else: 
+            texto1='Izquierda'
+        st.success(texto1)
+
+def predict(model,tokenizer, input_text):
+    inputs = tokenizer(input_text, return_tensors="pt", padding='max_length', truncation=True,max_length=MAX_SEQ)
+    outputs = model(**inputs)
+    #outputs=model(inputs)
+    logits = outputs.logits
+    _, preds = torch.max(logits, 1)
+    return preds.item()
+
+def printHeader(model,tokenizer):
+    
+    st.title('Interface de Usuario para Text Classification')
+    st.text(
+    '''A continuación tiene un espacio para escribir un texto de hasta 3000 caracteres 
+    (unas 500 palabras).  Una vez escrito pulse sobre el botón asociado y el sistema 
+    predecirá un sentimiento político siginificando 0 izquierda y 1 derecha. 
+    En el caso de que el texto sea más largo suba un fichero en formato txt''')
+
+    with st.form(key='my_form'):
+        #text_input = st.text_input(label='Enter some text',max_chars=600)
+        text_area= st.text_area (
+            label="Entre aqui el texto:",
+            height=150,
+            placeholder="Texto...",
+            max_chars=3000)
+        submitted = st.form_submit_button(label='Submit')
+    if submitted:
+        spinnerWidget(model,tokenizer,text_area)
+
+
+    uploaded_file = st.file_uploader("", type=["txt", "csv", "pdf"])
+ 
+    if uploaded_file is not None:
+    # Verifica el tamaño del archivo
+        file_size = uploaded_file.size  # Tamaño en bytes
+        max_size = 10 * 1024  # 10K MB en bytes
+        if file_size > max_size:
+            st.error(f"El archivo excede el tamaño máximo permitido de 10K. Tamaño actual:\
+                {file_size / (1024):.2f} KB")
+        else:
+            # Lee el contenido del archivo
+            texto = ""
+    # Leemos el contenido del archivo
+            contenido = uploaded_file.getvalue().decode('utf-8')
+            contenidoList=formatContent(contenido)
+            derecha=0
+            izquierda=0
+            for contenido in contenidoList:
+                texto=""
+                for line in contenido.splitlines():
+                    texto += line + "\n"
+                resultado=predict(model,tokenizer,texto)   
+                if resultado == 1:
+                    derecha+=1
+                else: 
+                    izquierda+=1
+            texto+=f'Izquierda={izquierda} Derecha={derecha}'
+            text_area=st.text_area("Contenido del archivo:", texto, height=300)
+            if derecha>izquierda:
+                st.text('Derecha')
+            elif derecha<izquierda:
+                st.text('Izquierda')
+            else:
+                st.text('Para ti albert rivera era el nuevo Kennedy. Pero le perdió la cabeza')
+            #resultado = predict(model,tokenizer,contenido)
+
+#f1=rutaRaiz+'\\modelo\\roberta_model\\modelorob.pth'
+f1=rutaRaiz+'\\modelo\\roberta_model\\modelstatedict.pth'
+#modelo=torch.load(f1, map_location=torch.device('cpu'))
+
+modelo=RobertaForSequenceClassification.from_pretrained(PRE_TRAINED_MODEL_NAME)
+state_dict=torch.hub.load_state_dict_from_url(urlm, map_location=torch.device('cpu'), check_hash=True)
+modelo.load_state_dict(state_dict)
+f2=rutaRaiz+'\\modelo\\roberta_model\\tokrob.pth'
+tokenizer=torch.load(f2)
+modelo.eval()
+printHeader(modelo,tokenizer)
