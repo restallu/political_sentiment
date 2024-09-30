46ed776a7d8171f4949a2f027b74941b4c790b39
restallu <74469347+restallu@users.noreply.github.com>
2024-09-30 21:20:04 +0200

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -61,7 +61,7 @@ def printHeader(model,tokenizer):
     if submitted:
         spinnerWidget(model,tokenizer,text_area)
 
-
+    
     uploaded_file = st.file_uploader("", type=["txt", "csv", "pdf"])
  
     if uploaded_file is not None:
@@ -79,29 +79,30 @@ def printHeader(model,tokenizer):
             contenidoList=formatContent(contenido)
             derecha=0
             izquierda=0
-            for contenido in contenidoList:
-                texto=""
-                for line in contenido.splitlines():
-                    texto += line + "\n"
-                resultado=predict(model,tokenizer,texto)   
-                if resultado == 1:
-                    derecha+=1
-                elif resultado==0: 
-                    izquierda+=1
+            with st.spinner('La frase tiene un sentimiento de.... '):  
+                for contenido in contenidoList:
+                    texto=""
+                    for line in contenido.splitlines():
+                        texto += line + "\n"
+                    resultado=predict(model,tokenizer,texto)   
+                    if resultado == 1:
+                        derecha+=1
+                    elif resultado==0: 
+                        izquierda+=1
+                    else:
+                        assert 0
+                texto+=f'Izquierda={izquierda} Derecha={derecha}'
+                if derecha+izquierda>1: #Se  divide  el  texto en bloques
+                    texto="Se muestra el ultimo bloque solamente\n"+texto
+                text_area=st.text_area("Contenido del archivo:", texto, height=300)
+                if derecha>izquierda:
+                    resultadotxt='DERECHA'
+                elif derecha<izquierda:
+                    resultadotxt='IZQUIERDA'
                 else:
-                    assert 0
-            texto+=f'Izquierda={izquierda} Derecha={derecha}'
-            if derecha+izquierda>1: #Se  divide  el  texto en bloques
-                texto="Se muestra el ultimo bloque solamente\n"+texto
-            text_area=st.text_area("Contenido del archivo:", texto, height=300)
-            if derecha>izquierda:
-                st.text('Derecha')
-            elif derecha<izquierda:
-                st.text('Izquierda')
-            else:
-                st.text('Para ti albert rivera era el nuevo Kennedy. Pero le perdió la cabeza')
-           
-
+                    resultadotxt='Para ti albert rivera era el nuevo Kennedy. Pero le perdió la cabeza'
+               st.success(resultadotxt)
+                
 
 f1='./modelorob.pth'
 modelo=torch.load(f1,map_location=torch.device('cpu'),weights_only=False)
